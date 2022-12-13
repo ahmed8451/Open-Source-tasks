1ee623b8dba8d423e1b34a7ef8248a8aea0980d0
Mahmoud Ali <79024928+mahmoudali2233@users.noreply.github.com>
2022-12-13 21:34:32 +0200

    done

--- Program.cs
+++ Program.cs
@@ -54,49 +54,39 @@ public class Database{
 public class Program{
     private static void Main(){
         var database = new Database();
-        Console.Write("Option: ");
-        var option = Convert.ToInt32(Console.ReadLine());
-        if (option == 1){
-            Console.Write("Name: ");
-            var name = Console.ReadLine();
-            Console.Write("Age: ");
-            var age = Convert.ToInt32(Console.ReadLine());
-            Console.Write("Year: ");
-            var year = Convert.ToInt32(Console.ReadLine());
-            Console.Write("Gpa: ");
-            var gpa = Convert.ToInt32(Console.ReadLine());
-            var student = new Student(name, age, year, gpa);
-            database.AddStudent(student);
-        }
-        else if (option ==2){
-            Console.Write("Name: ");
-            var name = Console.ReadLine();
-            Console.Write("Age: ");
-            var age = Convert.ToInt32(Console.ReadLine());
-            Console.Write("Slary: ");
-            var salary = Convert.ToInt32(Console.ReadLine());
-            Console.Write("JoinYear: ");
-            var joinyear = Convert.ToInt32(Console.ReadLine());
-            var staff = new Staff(name, age, salary, joinyear);
-            database.AddStaff(staff);
-        }
-        else if (option == 3){
-            Console.Write("Name: ");
-            var name = Console.ReadLine();
-            Console.Write("Age: ");
-            var age = Convert.ToInt32(Console.ReadLine());
-            Console.Write("Year: ");
-            var year = Convert.ToInt32(Console.ReadLine());
-            Console.Write("Gpa: ");
-            var gpa = Convert.ToInt32(Console.ReadLine());
-            Console.Write("Slary: ");
-            var salary = Convert.ToInt32(Console.ReadLine());
-            Console.Write("JoinYear: ");
-            var joinyear = Convert.ToInt32(Console.ReadLine());
-            var student = new Student(name, age, year, gpa);
-            database.AddStudent(student);
-            var staff = new Staff(name, age, salary, joinyear);
-            database.AddStaff(staff);
+        while (true) {
+            Console.Write("Option: ");
+            var option = Convert.ToInt32(Console.ReadLine());
+            if (option == 1){
+                Console.Write("Name: ");
+                var name = Console.ReadLine();
+                Console.Write("Age: ");
+                var age = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Year: ");
+                var year = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Gpa: ");
+                var gpa = Convert.ToInt32(Console.ReadLine());
+                var student = new Student(name, age, year, gpa);
+                database.AddStudent(student);
+            }
+            else if (option ==2){
+                Console.Write("Name: ");
+                var name = Console.ReadLine();
+                Console.Write("Age: ");
+                var age = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Slary: ");
+                var salary = Convert.ToInt32(Console.ReadLine());
+                Console.Write("JoinYear: ");
+                var joinyear = Convert.ToInt32(Console.ReadLine());
+                var staff = new Staff(name, age, salary, joinyear);
+                database.AddStaff(staff);
+            }
+            else if (option == 3){
+                foreach(var item in People)
+                {
+                    Console.WriteLine(item.ToString());
+                }
+            }
         }
     }
 }
