528863bb3e033c3ef5ae01c403d6be12df9d8d9c
Mahmoud Ali <79024928+mahmoudali2233@users.noreply.github.com>
2022-12-13 20:18:03 +0200

    csharp

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+// comment out the
+/* comment */
+namespace Rectangle{
+    class Rectangle{
+        double lenght;
+        double width;
+    
+        public void Values(){
+            lenght = 4;
+            width = 5;
+        }
+        public double GetArea(){
+            return lenght * width;
+        }
+        public void Display(){
+            Console.WriteLine("Lenght: " + lenght);
+            Console.WriteLine("Width: " + width);
+            Console.WriteLine("Area: "+ GetArea());
+        }
+    }
+    class ExcuteRectangle{
+        static void Main(string[] args){
+            Rectangle r = new Rectangle();
+            r.Values();
+            r.Display();
+            Console.ReadLine();
+        }
+    }
+}
