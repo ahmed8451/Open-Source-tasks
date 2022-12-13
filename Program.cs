bc6e37d84f5dd82e032a87002bad73ba47dc6031
Mahmoud Ali <79024928+mahmoudali2233@users.noreply.github.com>
2022-12-13 21:22:18 +0200

    done

--- Program.cs
+++ Program.cs
@@ -54,19 +54,36 @@ public class Database{
 public class Program{
     private static void Main(){
         var database = new Database();
-        Console.Write("Name: ");
-        var name = Console.ReadLine();
-        Console.Write("Age: ");
-        var age = Convert.ToInt32(Console.ReadLine());
-        Console.Write("Year: ");
-        var year = Convert.ToInt32(Console.ReadLine());
-        Console.Write("Gpa: ");
-        var gpa = Convert.ToInt32(Console.ReadLine());
-        Console.Write("Slary: ");
-        var salary = Convert.ToInt32(Console.ReadLine());
-        Console.Write("JoinYear: ");
-        var joinyear = Convert.ToInt32(Console.ReadLine());
-        var student = new Student(name, age, year, gpa);
-        database.AddStudent(student);
+        Console.Write("Option: ");
+        var option = Convert.ToInt32(Console.ReadLine());
+        if (option == 1){
+            Console.Write("Name: ");
+            var name = Console.ReadLine();
+            Console.Write("Age: ");
+            var age = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Year: ");
+            var year = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Gpa: ");
+            var gpa = Convert.ToInt32(Console.ReadLine());
+            var student = new Student(name, age, year, gpa);
+            database.AddStudent(student);
+        }
+        else if (option ==2){
+            var name = Console.ReadLine();
+            Console.Write("Age: ");
+            var age = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Year: ");
+            var year = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Slary: ");
+            var salary = Convert.ToInt32(Console.ReadLine());
+            Console.Write("JoinYear: ");
+            var joinyear = Convert.ToInt32(Console.ReadLine());
+            var staff = new Staff(name, age, salary, joinyear);
+            database.AddStaff(staff);
+        }
+        
+        
+        
+        
     }
 }
